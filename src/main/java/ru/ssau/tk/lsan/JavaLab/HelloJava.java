8d8a7693c020641955f4efb5c166c18672c53ab2
L-san <lsarelli9402@gmail.com>
2020-09-08 22:59:13 +0400

    Создан экземпляр класса Person

--- src/main/java/ru/ssau/tk/lsan/JavaLab/HelloJava.java
+++ src/main/java/ru/ssau/tk/lsan/JavaLab/HelloJava.java
@@ -1,8 +1,14 @@
 package ru.ssau.tk.lsan.JavaLab;
 
 public class HelloJava {
-
+//TODO отдельные файлы для задачек, видимо. А то мэйн один, а вас много
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Person lSan = new Person();
+        lSan.setFirstName("Lyudmila");
+        lSan.setLastName("Seleznyova");
+        lSan.setPassportId(5);
+        System.out.println(lSan.getFirstName());
+        System.out.println(lSan.getLastName());
+        System.out.println(lSan.getPassportId());
     }
 }
