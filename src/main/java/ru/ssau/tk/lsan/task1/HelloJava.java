2bddfbffe0c27c58bf0bdbc14c9a204c79ca843a
L-san <lsarelli9402@gmail.com>
2020-09-09 21:10:03 +0400

    created 4 instances of the "Person" class and...
    in debug some fields of instances without parameters had "null"
    it's ok

--- src/main/java/ru/ssau/tk/lsan/task1/HelloJava.java
+++ src/main/java/ru/ssau/tk/lsan/task1/HelloJava.java
@@ -3,6 +3,7 @@ package ru.ssau.tk.lsan.task1;
 public class HelloJava {
 //TODO отдельные файлы для задачек, видимо. А то мэйн один, а вас много
     public static void main(String[] args) {
+        //task 1.1------------------------------------------------------
         Person lSan = new Person();
         lSan.setFirstName("Lyudmila");
         lSan.setLastName("Seleznyova");
@@ -10,5 +11,13 @@ public class HelloJava {
         System.out.println(lSan.getFirstName());
         System.out.println(lSan.getLastName());
         System.out.println(lSan.getPassportId());
+        //--------------------------------------------------------------
+
+        //task 1.2------------------------------------------------------
+        Person human1 = new Person();
+        Person human2 = new Person("firstName","lastName");
+        Person human3 = new Person(5);
+        Person human4 = new Person("firstName","lastName",5);
+        //--------------------------------------------------------------
     }
 }
