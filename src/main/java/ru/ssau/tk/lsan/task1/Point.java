f9eb9a64e3b6306df16cc104dfeac5f95dbffdd6
L-san <lsarelli9402@gmail.com>
2020-09-10 20:02:56 +0400

    created Point class

---
+++ src/main/java/ru/ssau/tk/lsan/task1/Point.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.lsan.task1;
+
+public class Point {
+    public final double X;
+    public final double Y;
+    public final double Z;
+
+    public Point(double X, double Y, double Z){
+        this.X = X;
+        this.Y = Y;
+        this.Z = Z;
+    }
+}
