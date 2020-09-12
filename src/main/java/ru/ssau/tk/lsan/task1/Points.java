8d0eb75ad02eb2a581df83c53951308f628afd92
L-san <lsarelli9402@gmail.com>
2020-09-12 18:47:30 +0400

    added substract

--- src/main/java/ru/ssau/tk/lsan/task1/Points.java
+++ src/main/java/ru/ssau/tk/lsan/task1/Points.java
@@ -6,4 +6,10 @@ public class Points {
                          firstPoint.Y+secondPoint.Y,
                          firstPoint.Z+secondPoint.Z);
     }
+
+    public static Point substract(Point firstPoint, Point secondPoint){
+        return new Point(firstPoint.X-secondPoint.X,
+                         firstPoint.Y-secondPoint.Y,
+                         firstPoint.Z-secondPoint.Z);
+    }
 }
