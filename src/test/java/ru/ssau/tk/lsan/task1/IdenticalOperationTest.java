6e0ced83bfe551a683f1096c3b32369d50e9abd0
L-san <lsarelli9402@gmail.com>
2020-09-23 19:33:53 +0400

    added test for negative numbers

--- src/test/java/ru/ssau/tk/lsan/task1/IdenticalOperationTest.java
+++ src/test/java/ru/ssau/tk/lsan/task1/IdenticalOperationTest.java
@@ -12,5 +12,6 @@ public class IdenticalOperationTest {
     public void testApply() {
         assertEquals(testFunc.apply(Double.POSITIVE_INFINITY), Double.POSITIVE_INFINITY);
         assertEquals(testFunc.apply(Double.NEGATIVE_INFINITY), Double.NEGATIVE_INFINITY);
+        assertEquals(testFunc.apply(-1),-1,0.001);
     }
 }
