8f9c2b12c7433eeebeaed497e8b3a7dab9ce3d31
L-san <lsarelli9402@gmail.com>
2020-09-23 19:31:52 +0400

    added test for positive infinity

--- src/test/java/ru/ssau/tk/lsan/task1/IdenticalOperationTest.java
+++ src/test/java/ru/ssau/tk/lsan/task1/IdenticalOperationTest.java
@@ -3,10 +3,13 @@ package ru.ssau.tk.lsan.task1;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
+
 @Test
 public class IdenticalOperationTest {
+    Operation posInf = new IdenticalOperation();
 
     @Test
     public void testApply() {
+        assertEquals(posInf.apply(Double.POSITIVE_INFINITY), Double.POSITIVE_INFINITY);
     }
 }
