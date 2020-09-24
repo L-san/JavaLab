133a32bed267fb64cb26af5db5afb233f6c45c6b
L-san <lsarelli9402@gmail.com>
2020-09-25 00:27:08 +0400

    added test for NaN

--- src/test/java/ru/ssau/tk/lsan/task1/SqrtOperationTest.java
+++ src/test/java/ru/ssau/tk/lsan/task1/SqrtOperationTest.java
@@ -13,5 +13,6 @@ public class SqrtOperationTest {
         assertEquals(testFunc.apply(4), 2, 0.001);
         assertEquals(testFunc.apply(Double.POSITIVE_INFINITY),Double.POSITIVE_INFINITY);
         assertNotEquals(testFunc.apply(Double.NEGATIVE_INFINITY),Double.NEGATIVE_INFINITY);
+        assertEquals(testFunc.apply(Double.NaN),Double.NaN);
     }
 }
