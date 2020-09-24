0131045bd87b0bbffcd51fd47e9acabb32a96922
L-san <lsarelli9402@gmail.com>
2020-09-25 00:21:06 +0400

    created test for SqrtOperation class

---
+++ src/test/java/ru/ssau/tk/lsan/task1/SqrtOperationTest.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.lsan.task1;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SqrtOperationTest {
+
+    @Test
+    public void testApply() {
+    }
+}
