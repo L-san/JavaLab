756441d8d3b3e8a86937334a9512847f0e4a2fd3
L-san <lsarelli9402@gmail.com>
2020-09-25 00:23:43 +0400

    added test for simple numbers

--- src/test/java/ru/ssau/tk/lsan/task1/SqrtOperationTest.java
+++ src/test/java/ru/ssau/tk/lsan/task1/SqrtOperationTest.java
@@ -4,9 +4,13 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
+@Test
 public class SqrtOperationTest {
+    Operation testFunc = new SqrtOperation();
 
     @Test
     public void testApply() {
+        assertEquals(testFunc.apply(4), 2, 0.001);
+
     }
 }
