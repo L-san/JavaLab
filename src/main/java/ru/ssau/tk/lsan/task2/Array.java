539a6dc0b4d29d577740b6daf709a60f1d403f37
L-san <lsarelli9402@gmail.com>
2020-10-06 12:21:05 +0400

    2nd task 2nd one
    Added interface ArrayMethods(just for convenience) for different array methods
    Added realization for methods newEmptyDoubleArray() and
    newFilledDoubleArray(int size)

--- src/main/java/ru/ssau/tk/lsan/task2/Array.java
+++ src/main/java/ru/ssau/tk/lsan/task2/Array.java
@@ -1,7 +1,19 @@
 package ru.ssau.tk.lsan.task2;
 
-public class Array {
-    public Double[] newDoubleArray(int size){
+public class Array implements ArrayMethods {
+    @Override
+    public Double[] newEmptyDoubleArray(int size) {
         return new Double[size];
+    }
+
+    @Override
+    public Double[] newFilledDoubleArray(int size) {
+            Double[] arr = new Double[size];
+            arr[0] = 2d;
+            arr[size-1] = 2d;
+            for (int i = 1; i < size-1; i++) {
+                arr[i] = 1d;
+            }
+            return arr;
     }
 }
---
+++ src/main/java/ru/ssau/tk/lsan/task2/ArrayMethods.java
@@ -0,0 +1,6 @@
+package ru.ssau.tk.lsan.task2;
+
+public interface ArrayMethods {
+    public Double[] newEmptyDoubleArray(int size);
+    public Double[] newFilledDoubleArray(int size);
+}
