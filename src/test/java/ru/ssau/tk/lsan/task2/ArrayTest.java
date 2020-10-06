d339780cd4db5749abc1711982a811920c4d6695
L-san <lsarelli9402@gmail.com>
2020-10-06 12:22:31 +0400

    updated tests for Array methods

--- src/test/java/ru/ssau/tk/lsan/task2/ArrayTest.java
+++ src/test/java/ru/ssau/tk/lsan/task2/ArrayTest.java
@@ -6,11 +6,22 @@ import static org.testng.Assert.*;
 
 public class ArrayTest {
     Array testArr = new Array();
+    protected final static int size = 6;
+    protected final static double delta = 0.001;
+    protected final Double[] newArr = new Array().newFilledDoubleArray(size);
 
     @Test
-    public void testNewDoubleArray() {
+    public void testNewEmptyDoubleArray() {
+        assertEquals(testArr.newEmptyDoubleArray(2).length, 2);
+        assertEquals(testArr.newEmptyDoubleArray(0).length, 0);
+    }
 
-        assertEquals(testArr.newDoubleArray(2).length, 2);
-        assertEquals(testArr.newDoubleArray(0).length, 0);
+    @Test
+    public void testNewFilledDoubleArray() {
+        assertEquals(newArr[0], 2d, delta);
+        assertEquals(newArr[size - 1], 2d, delta);
+        for (int i = 1; i < size - 2; i++) {
+            assertEquals(newArr[i], 1d, delta);
+        }
     }
 }
