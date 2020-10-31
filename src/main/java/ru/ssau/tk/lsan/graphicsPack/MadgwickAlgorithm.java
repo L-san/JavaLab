0e46749b799e81b79a83510c983a8b567edf88b4
L-san <lsarelli9402@gmail.com>
2020-10-31 11:41:06 +0400

    Алгоритм дописан, но еще не проверен
    поля для переменных - приватны, доступ извне только к q_est - добавлен геттер;
    Добавлен конструктор для инициализации НУ алгоритма
    Переопределения для сумм и умножений

--- src/main/java/ru/ssau/tk/lsan/graphicsPack/MadgwickAlgorithm.java
+++ src/main/java/ru/ssau/tk/lsan/graphicsPack/MadgwickAlgorithm.java
@@ -1,11 +1,29 @@
 package ru.ssau.tk.lsan.graphicsPack;
 
 public class MadgwickAlgorithm {
-    private double q_init;
-    private final double dzta = 0;
+    private final double dzta;
+    private final double bta;
+    private double[] q_est;
+    private double[] q_omega;
+    private double omega_eps_prev;
+    private double[] q_eps_dot;
 
     MadgwickAlgorithm() {
+        this.bta = 0d;
+        this.dzta = 0d;
+    }
 
+    MadgwickAlgorithm(double[] q_est_0, double[] q_omega_0, double omega_eps_prev_0, double[] q_eps_dot_0, double bta0, double dzta0) {
+        this.bta = bta0;
+        this.dzta = dzta0;
+        this.q_est = q_est_0;
+        this.q_omega = q_omega_0;
+        this.omega_eps_prev = omega_eps_prev_0;
+        this.q_eps_dot = q_eps_dot_0;
+    }
+
+    public double[] getQ_est() {
+        return q_est;
     }
 
     protected double dot(double[] a, double[] b) {
@@ -49,6 +67,18 @@ public class MadgwickAlgorithm {
         return c;
     }
 
+    protected double[] matrixMultiplication(double[][] a, double[] bt) {
+        //A (l x m) и B (m x n) определяется как Матрица C (l x n)
+        double[][] b = new double[4][1];
+        b[0][0] = bt[0];
+        b[1][0] = bt[1];
+        b[2][0] = bt[2];
+        b[3][0] = bt[3];
+        double[][] c;
+        c = matrixMultiplication(a, b, 4, 6, 1);
+        return new double[]{c[0][0], c[1][0], c[2][0], c[3][0]};
+    }
+
     /*protected double[][] matrixMultiplication(double[][] a, double b, int l, int m) {
         double[][] c = new double[l][m];
         for (int i = 0; i < l; ++i) {
@@ -59,34 +89,50 @@ public class MadgwickAlgorithm {
         return c;
     }
     */
-    protected double[] matrixMultiplication(double[] a, double b, int h) {
-        double[] c = new double[h];
-        for (int i = 0; i < h; ++i) {
+    protected double[] matrixMultiplication(double[] a, double b) {
+        double[] c = new double[4];
+        for (int i = 0; i < 4; ++i) {
             c[i] = a[i] * b;
         }
         return c;
     }
 
-  /*  protected double[][] matrixSum(double[][] a, double[][] b, int h, int w) {
-        double[][] c = new double[][]{};
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                c[i][j] = a[i][j] + b[i][j];
-            }
+    /*  protected double[][] matrixSum(double[][] a, double[][] b, int h, int w) {
+          double[][] c = new double[][]{};
+          for (int i = 0; i < h; i++) {
+              for (int j = 0; j < w; j++) {
+                  c[i][j] = a[i][j] + b[i][j];
+              }
+          }
+          return c;
+      }
+  */
+    protected double[] matrixSum(double[] a, double[] b) {
+        double[] c = new double[a.length];
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i] + b[i];
         }
         return c;
     }
 
-    protected double[] matrixSum(double[] a, double[] b, int h) {
-        double[] c = new double[]{};
-        for (int i = 0; i < h; i++) {
-            c[i] = a[i] + b[i];
+    protected double[] normalizeVector(double[] a) {
+        double[] c = new double[a.length];
+        double norm = Math.sqrt(dot(a, a));
+        if (norm != 0) {
+            for (int i = 0; i < a.length; i++) {
+                c[i] = a[i] / norm;
+            }
+        } else {
+            for (int i = 0; i < a.length; i++) {
+                c[i] = 0;
+            }
         }
+
         return c;
-    }*/
+    }
 
     protected double[] matrixSum(double[] a, double b, int h) {
-        double[] c = new double[]{};
+        double[] c = new double[h];
         for (int i = 0; i < h; i++) {
             c[i] = a[i] + b;
         }
@@ -94,18 +140,20 @@ public class MadgwickAlgorithm {
     }
 
     protected double[] matrixDivision(double[] a, double[] b, int h) {
-        double[] c = new double[]{};
+        double[] c = new double[h];
         for (int i = 0; i < h; i++) {
             c[i] = a[i] - b[i];
         }
         return c;
     }
 
-    protected double calculatePosition(double[] q_est, double[] q_eps_dot, double[] a, double[] m, double[] g, double omega_eps_prev, double delta_T) {
+    protected void calculatePosition(double[] a, double[] m, double[] g, double delta_T) {
         double[] f_a = new double[3];
         double[][] J_a = new double[3][4];
         double[] f_m = new double[3];
         double[][] J_m = new double[3][4];
+        double[] q_omega_dot = new double[4];
+
         f_a[0] = 2 * (q_est[1] * q_est[3] - q_est[0] * q_est[2]) - a[0];
         f_a[1] = 2 * (q_est[0] * q_est[1] + q_est[2] * q_est[3]) - a[1];
         f_a[2] = 2 * (0.5 - q_est[1] * q_est[1] - q_est[2] * q_est[2]) - a[2];
@@ -145,23 +193,30 @@ public class MadgwickAlgorithm {
         J_m[2][3] = 2 * b[1] * q_est[1];
 
         double[][] J = new double[][]{
-                {J_a[0][0], J_a[1][0], J_a[2][0]},
-                {J_a[0][1], J_a[1][1], J_a[2][1]},
-                {J_a[0][2], J_a[1][2], J_a[2][2]},
-                {J_a[0][3], J_a[1][3], J_a[2][3]},
-                {J_m[0][0], J_m[1][0], J_m[2][0]},
-                {J_m[0][1], J_m[1][1], J_m[2][1]},
-                {J_m[0][2], J_m[1][2], J_m[2][2]},
-                {J_m[0][3], J_m[1][3], J_m[2][3]}};
-        double[][] f = new double[][]{{f_a[0], f_m[0]}, {f_a[1], f_m[1]}, {f_a[2], f_m[2]}};
-        double[][] grad_f = matrixMultiplication(J, f, 8, 3, 2);
+                {J_a[0][0], J_a[1][0], J_a[2][0], J_m[0][0], J_m[1][0], J_m[2][0]},
+                {J_a[0][1], J_a[1][1], J_a[2][1], J_m[0][1], J_m[1][1], J_m[2][1]},
+                {J_a[0][2], J_a[1][2], J_a[2][2], J_m[0][2], J_m[1][2], J_m[2][2],},
+                {J_a[0][3], J_a[1][3], J_a[2][3], J_m[0][3], J_m[1][3], J_m[2][3]}};
+
+        double[] f = new double[]{f_a[0], f_a[1], f_a[2], f_m[0], f_m[1], f_m[2]};
+        double[] grad_f = matrixMultiplication(J, f);
+        q_eps_dot = normalizeVector(grad_f);
 
         double[] omega_eps = quat_mult(quat_conj(q_est), q_eps_dot);
-        omega_eps = matrixMultiplication(omega_eps, 2 * delta_T, 4);
+        omega_eps = matrixMultiplication(omega_eps, 2 * delta_T);
         omega_eps = matrixSum(omega_eps, omega_eps_prev, 4);
         double[] omega_b = matrixSum(omega_eps, dzta, 4);
         double[] omega_c = matrixDivision(new double[]{0d, g[0], g[1], g[2]}, omega_b, 4);
 
-        return 0d;
+        q_omega_dot[0] = -0.5 * dot(new double[]{q_est[1], q_est[2], q_est[3]}, new double[]{omega_b[1], omega_b[2], omega_b[3]});
+        double[] crossQOmega = cross(new double[]{q_est[1], q_est[2], q_est[3]}, new double[]{omega_b[1], omega_b[2], omega_b[3]});
+        q_omega_dot[1] = 0.5 * (q_est[0] * omega_b[1] + crossQOmega[1]);
+        q_omega_dot[2] = 0.5 * (q_est[0] * omega_b[2] + crossQOmega[2]);
+        q_omega_dot[3] = 0.5 * (q_est[0] * omega_b[3] + crossQOmega[3]);
+        q_omega = matrixSum(q_omega, matrixMultiplication(q_omega_dot, delta_T));
+
+        double[] q_est_dot = matrixDivision(q_omega, matrixMultiplication(q_eps_dot, bta), 4);
+        q_est = matrixSum(q_est, matrixMultiplication(q_est_dot, delta_T));
+        q_est = normalizeVector(q_est);
     }
 }
