5065f60e8cea3b2f20702f43f3137963da2d9584
L-san <lsarelli9402@gmail.com>
2020-11-08 20:57:33 +0400

    Добавлен фильтр - работает ли? Пока не знаю
    Нужен отдельный интерфейс под графики, чтобы понять

---
+++ src/main/java/ru/ssau/tk/lsan/graphicsPack/algorithms/KalmanFilter.java
@@ -0,0 +1,38 @@
+package ru.ssau.tk.lsan.graphicsPack.algorithms;
+
+import ru.ssau.tk.lsan.graphicsPack.operations.LinearAlgebraOperations;
+
+public class KalmanFilter extends LinearAlgebraOperations {
+    private double[] x;
+    private double[][] P, F, H, Q, R;
+    private final int nx;
+
+    KalmanFilter(double[] x0,double Q0,double R0) {
+        this.x = x0;
+        this.nx = x0.length;
+        double[][] i = new double[][]{{1d,0d,0d},{0d,1d,0d},{0d,0d,1d}};
+        this.F = i;
+        this.H = i;
+        this.P = i;
+        this.Q = matrixMultiplication(i,Q0);
+        this.R = matrixMultiplication(i,R0);
+    }
+
+    public double[] getX() {
+        return x;
+    }
+
+    public void doFiltering(double[] z) {
+        //predict
+        x = matrixMultiplication(F, x);
+        P = matrixSum(matrixMultiplication(matrixMultiplication(F, P, nx, nx, nx), transponse(F), nx, nx, nx), Q);
+        //update
+        double[][] S;
+        S = matrixSum(matrixMultiplication(matrixMultiplication(H, P, nx, nx, nx), transponse(H), nx, nx, nx), R);
+        double[][] K;
+        K = matrixMultiplication(matrixMultiplication(P, transponse(H), nx, nx, nx), transponse(inv(S)), nx, nx, nx);
+        x = matrixSum(x, matrixMultiplication(K, matrixDivision(z, matrixMultiplication(H, x), nx)));
+        P = matrixDivision(P, matrixMultiplication(matrixMultiplication(P, K, nx, nx, nx), H, nx, nx, nx));
+    }
+
+}
