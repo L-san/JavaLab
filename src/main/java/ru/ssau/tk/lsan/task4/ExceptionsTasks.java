1f4bcab37abd5343eb677d3599d7c9a779f5d268
L-san <lsarelli9402@gmail.com>
2020-12-09 23:17:00 +0400

    4.4

---
+++ src/main/java/ru/ssau/tk/lsan/task4/ExceptionsTasks.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.lsan.task4;
+
+public class ExceptionsTasks {
+    public static int integerDivision(String a, String b){
+        return Integer.parseInt(a)/Integer.parseInt(b);
+    }
+}
